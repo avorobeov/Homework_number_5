59e905b7f41052e3938786c26bcd6701825a0f6c
воробйов александр <avorobeov21@gmail.com>
2023-04-14 00:21:55 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Воробйов";
+            string surname = "Александр";
+            string temporaryStorage;
+
+            Console.WriteLine($"{name} {surname}");
+
+            temporaryStorage = name;
+            name = surname;
+            surname = temporaryStorage;
+
+            Console.WriteLine($"{name} {surname}");
+            Console.ReadKey();
+        }
+    }
+}
